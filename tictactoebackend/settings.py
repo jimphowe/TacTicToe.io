a5300cccfa5d0f439264e91049e10864f0b837ca
Jim Howe <jim@jims-air-2.lan>
2024-05-08 19:22:00 -0400

    initialize postgres db tactictoe

--- tictactoebackend/settings.py
+++ tictactoebackend/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'tictactoebackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tactictoe',
+        'USER': 'jim',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
