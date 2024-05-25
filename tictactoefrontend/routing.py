2b632ae2a6b55126b21b0f8aae894a9a4558f630
Jim Howe <jim@Jims-MacBook-Air-2.local>
2024-05-25 18:04:18 -0400

    ws working, breaks on game over

--- tictactoefrontend/routing.py
+++ tictactoefrontend/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from .consumers import GameConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/game/(?P<game_id>\w+)/$', GameConsumer.as_asgi()),
+    re_path(r'^ws/game/(?P<game_id>\d+)/$', GameConsumer.as_asgi()),
 ]
+#'ws://' + window.location.host + '/ws/game/' + '39' + '/'
