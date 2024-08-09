df9cb777614e78f48fb0084e856c3d07aff7f0cd
Jim Howe <jim@jims-air-2.lan>
2024-08-09 18:43:38 -0400

    add csrf trusted origins

--- config/settings.py
+++ config/settings.py
@@ -181,3 +181,9 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://tactictoe.io', 'https://www.tactictoe.io']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
