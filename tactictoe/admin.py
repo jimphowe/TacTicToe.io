9e3908a0a72834bd3929bf1b76d76cf8d33f727d
Jim Howe <jim@jims-air-2.lan>
2024-10-01 11:58:20 -0400

    register models

--- tactictoe/admin.py
+++ tactictoe/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile, EloRating, Game
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(EloRating)
+admin.site.register(Game)
